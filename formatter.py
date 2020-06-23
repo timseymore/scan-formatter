55656fb20b5baf1b626fa573afb9cd3e66f1e608
Tim Seymore <tim_seymore2012@hotmail.com>
2020-06-23 18:02:31 -0400

    Initial commit

---
+++ formatter.py
@@ -0,0 +1,35 @@
+"""
+SCAN FORMATTER
+
+Tool used to format a .txt file associated with a scan tool.
+
+- Expects a file named "s.txt" to be present in same directory as formatter.py
+- Copies txt from s.txt and appends it to the current date and a given repair order number
+- Creates scan.txt with the current date, r.o. number, and scan info
+- Deletes s.txt when finished to avoid problems with next scan file used
+
+
+MIT License
+
+Copyright (c) 2020 Tim Seymore
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+"""
+
